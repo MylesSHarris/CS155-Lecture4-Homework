6835b070f7a5922f35f5c0329c16cc1157011ce9
MylesSHarris <69069959+MylesSHarris@users.noreply.github.com>
2020-10-06 09:19:25 -0700

    commit

---
+++ Part1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double weight = Double.Parse(weightText.Text);
+            double height = Double.Parse(heightText.Text);
+
+            double bodyMassIndex = (weight * 720) / Math.Pow(height, 2);
+
+            if (bodyMassIndex < 15)
+            {
+                healthyText.Background = Brushes.Blue;
+                healthyText.Text = "Below healthy range.";
+            }
+            else if (bodyMassIndex > 26)
+            {
+                healthyText.Background = Brushes.Yellow;
+                healthyText.Text = "Above healthy range.";
+            } else
+            {
+                healthyText.Background = Brushes.Green;
+                healthyText.Text = "Within healthy range.";
+            }
+            
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heads = 0, tails = 0;
+
+            Console.WriteLine("For each coin toss enter either 'h' for heads or 't' for tails.");
+            for (int i=1; i<=8; i++)
+            {
+                Console.Write($"Enter result for toss {i}:");
+                if (Console.ReadLine().ToLower().Equals("h"))
+                {
+                    heads++;
+                }
+                else
+                {
+                    tails++;
+                }
+            }
+            Console.WriteLine($"Number of heads: {heads}");
+            Console.WriteLine($"Number of tails: {tails}");
+
+            double percentHeads = heads * 100.0 / (heads + tails);
+            double percentTails = tails * 100.0 / (heads + tails);
+
+            Console.WriteLine($"Percent heads: {percentHeads}");
+            Console.WriteLine($"Percent tails: {percentTails}");
+        }
+    }
+}
